6c6c39acbc992a8123d71d3ce46319eb4e3fc0e8
SakshyatSapkota <152952897+SakshyatSapkota@users.noreply.github.com>
2024-06-27 23:02:21 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var menuICon = document.querySelector(".menu-icon");
+var sidebar = document.querySelector(".sidebar");
+var container = document.querySelector(".container");
+
+menuICon.onclick = function(){
+    sidebar.classList.toggle("small-sidebar");
+    container.classList.toggle("large-container");
+}
+
+
